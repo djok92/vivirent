8ee4f6e7641d5d61a1adeeab83831fb77dc5bba0
djok92 <stefan.djokic92@gmail.com>
2018-05-23 18:16:49 +0200

    ...

--- parts/section-apartments.php
+++ parts/section-apartments.php
@@ -27,52 +27,52 @@
 		if($query->have_posts()) {
 			while($query->have_posts()) {
 				$query->the_post(); ?>
-                <div class="row">
-                    <div class="apartments-box">
-                        <div class="col-md-4">
-                            <div class="apartments-box_img">
-								<?php if(get_the_post_thumbnail()) {
-									the_post_thumbnail();
-								} else { ?>
-                                    <a href="<?php the_permalink(); ?> ">
-                                        <img src="<?php the_field('defimg', 'options'); ?>"/>
-                                    </a>
-								<?php } ?>
-                            </div>
-                        </div><!-- /.col-md-4 -->
-                        <div class="col-md-8">
-                            <div class="apartments-box__content">
+                <div class="apartments-box">
+                    <div class="row">
+                            <div class="col-md-4">
+                                <div class="apartments-box_img">
+                                    <?php if(get_the_post_thumbnail()) {
+                                        the_post_thumbnail();
+                                    } else { ?>
+                                        <a href="<?php the_permalink(); ?> ">
+                                            <img src="<?php the_field('defimg', 'options'); ?>"/>
+                                        </a>
+                                    <?php } ?>
+                                </div>
+                            </div><!-- /.col-md-4 -->
+                            <div class="col-md-8">
+                                <div class="apartments-box__content">
 
-                                <p class="apartments-box__content_name">
-									<?php $terms = get_the_terms($post->ID, 'vila');
-									foreach($terms as $term) {
-										echo $term->name;
-									} ?>
-                                </p>
+                                    <p class="apartments-box__content_name">
+                                        <?php $terms = get_the_terms($post->ID, 'vila');
+                                        foreach($terms as $term) {
+                                            echo $term->name;
+                                        } ?>
+                                    </p>
 
-                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
-<!--                                <div class="apartments-box__content_specification">-->
-<!--                                    <p>--><?php //_e('Karakteristike', 'wpog'); ?><!--:</p>-->
-<!--                                    <span>--><?php //_e('max broj osoba', 'wpog'); ?>
-<!--                                        : --><?php //the_field('broj_gostiju'); ?><!--</span>,-->
-<!--                                    <span>--><?php //the_field('kvadratura'); ?><!--</span>,-->
-<!--                                    <span>--><?php //the_field('soba'); ?><!----><?php //_e('sobe', 'wpog'); ?><!--</span>,-->
-<!--                                    <span>--><?php //the_field('kupaonica'); ?><!----><?php //_e('kupatila', 'wpog'); ?><!--</span>-->
-<!--                                </div><!-- /.apartments-box__content_specification -->
+    <!--                                <div class="apartments-box__content_specification">-->
+    <!--                                    <p>--><?php //_e('Karakteristike', 'wpog'); ?><!--:</p>-->
+    <!--                                    <span>--><?php //_e('max broj osoba', 'wpog'); ?>
+    <!--                                        : --><?php //the_field('broj_gostiju'); ?><!--</span>,-->
+    <!--                                    <span>--><?php //the_field('kvadratura'); ?><!--</span>,-->
+    <!--                                    <span>--><?php //the_field('soba'); ?><!----><?php //_e('sobe', 'wpog'); ?><!--</span>,-->
+    <!--                                    <span>--><?php //the_field('kupaonica'); ?><!----><?php //_e('kupatila', 'wpog'); ?><!--</span>-->
+    <!--                                </div><!-- /.apartments-box__content_specification -->
 
-                                <div class="apartments-box__content_links">
-                                    <a href="<?php the_permalink(); ?>"
-                                       class="link black"><?php _e('više informacija', 'wpog'); ?></a>
-                                    <a href="<?php if(function_exists('af_booking')) {
-										echo add_query_arg(array('acm_id' => $post->ID), af_booking()->getBookingURL());
-									} ?>" class="link"><?php _e('rezerviši', 'wpog'); ?></a>
-                                </div>  <!-- /.apartments-box__content_links -->
+                                    <div class="apartments-box__content_links">
+                                        <a href="<?php the_permalink(); ?>"
+                                        class="link black"><?php _e('više informacija', 'wpog'); ?></a>
+                                        <a href="<?php if(function_exists('af_booking')) {
+                                            echo add_query_arg(array('acm_id' => $post->ID), af_booking()->getBookingURL());
+                                        } ?>" class="link"><?php _e('rezerviši', 'wpog'); ?></a>
+                                    </div>  <!-- /.apartments-box__content_links -->
 
-                            </div> <!-- /.apartments-box__content -->
-                        </div><!-- /.col-md-8 -->
-                    </div> <!-- /.apartments-box -->
-                </div><!-- /.row -->
+                                </div> <!-- /.apartments-box__content -->
+                            </div><!-- /.col-md-8 -->
+                    </div><!-- /.row -->
+                </div> <!-- /.apartments-box -->
 			<?php }
 		} else { ?>
 			<?php printf(__('Stranica nije dostupna. Vratite se na <a href="%s">početnu stranu</a>.', 'wpog'), home_url()); ?>
